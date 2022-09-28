40a4dc3097d7cf3f65dc1ce6b74b8ddc69d4ccc4
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-28 22:55:39 +0000

    Ensured that correct game detail is shown with no errors. #2

--- js/main.js
+++ js/main.js
@@ -11,26 +11,26 @@ var $backButton = document.querySelector('.back-button');
 var $nextButton = document.querySelector('.next-button');
 var $pageNumberTop = document.querySelector('.page-number-top');
 var $pageNumberBot = document.querySelector('.page-number-bot');
+var view = 'featured';
 var pageNumber = 1;
-data.view = 'featured'; // Temp
 
 function getData(url) {
   var xhr = new XMLHttpRequest();
 
-  if (data.view === 'featured') {
+  if (view === 'featured') {
     xhr.open('GET', domain + key + pageParam + pageNumber.toString());
-  } else if (data.view === 'detail') {
+  } else if (view === 'detail') {
     xhr.open('GET', url);
   }
 
   xhr.responseType = 'json';
 
   xhr.addEventListener('load', function (event) {
-    if (data.view === 'featured') {
+    if (view === 'featured') {
       renderCards(xhr.response.results);
       pageUrl = xhr.response.next;
-    } else if (data.view === 'detail') {
-      // fillDetail(xhr.response);
+    } else if (view === 'detail') {
+      fillDetail(xhr.response);
     }
   });
 
@@ -82,14 +82,54 @@ function renderCards(array) {
   }
 }
 
-// function fillDetail(object) {
+function fillDetail(object) {
+  var $title = document.querySelector('.title');
+  var $thumbnail = document.querySelector('.thumbnail-detail');
+  var $about = document.querySelector('.about');
+  var $genre = document.querySelector('.genre');
+  var $releaseDate = document.querySelector('.release-date');
+  var $developer = document.querySelector('.developer');
+  var $publisher = document.querySelector('.publisher');
+  var $esrbRating = document.querySelector('.esrb-rating');
+  var $website = document.querySelector('.website');
 
-// }
+  $genre.replaceChildren();
+  $developer.replaceChildren();
+  $publisher.replaceChildren();
+
+  $title.textContent = object.name;
+  $thumbnail.src = object.background_image;
+  $thumbnail.alt = object.name;
+  $about.textContent = object.description_raw;
+
+  for (var i = 0; i < object.genres.length; i++) {
+    var el1 = document.createElement('li');
+    el1.textContent = object.genres[i].name;
+    $genre.appendChild(el1);
+  }
+
+  $releaseDate.textContent = object.released;
+
+  for (var j = 0; j < object.developers.length; j++) {
+    var el2 = document.createElement('li');
+    el2.textContent = object.developers[j].name;
+    $developer.appendChild(el2);
+  }
+
+  for (var k = 0; k < object.publishers.length; k++) {
+    var el3 = document.createElement('li');
+    el3.textContent = object.publishers[k].name;
+    $publisher.appendChild(el3);
+  }
+
+  $esrbRating.textContent = object.esrb_rating.name;
+  $website.href = object.website;
+}
 
 $cards.addEventListener('click', function (event) {
   if (event.target.closest('.card-featured')) {
     gameUrl = event.target.closest('.card-featured').getAttribute('data-url');
-    data.view = 'detail';
+    view = 'detail';
     $featuredView.hidden = true;
     $detailView.hidden = false;
 
@@ -98,7 +138,7 @@ $cards.addEventListener('click', function (event) {
 });
 
 $backLink.addEventListener('click', function (event) {
-  data.view = 'featured';
+  view = 'featured';
   $featuredView.hidden = false;
   $detailView.hidden = true;
 });

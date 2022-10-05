484c41db44f91c97194e3a9d76b378f05c382dbd
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-05 06:57:10 +0000

    Refined functions for AJAX and DOM tree creation.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ var currentDetail = {
   name: null,
   slug: null
 };
+var placeholderImage = 'https://via.placeholder.com/200x200.jpg?text=+';
 
 var $bookmarkIconHeader = document.querySelector('.bookmarks');
 var $searchIcon = document.querySelector('.search');
@@ -22,57 +23,65 @@ var $backLinkFeat = document.querySelector('.back-arrow-featured');
 var $viewLabel = document.querySelector('.view-label');
 var $pageLabel = document.querySelector('.page-label');
 var $pageNumTop = document.querySelector('.page-number-top');
-var $cards = document.querySelector('.cards');
+var $featuredGames = document.querySelector('.featured-games');
 var $backButton = document.querySelector('.back-button');
 var $pageNumBot = document.querySelector('.page-number-bot');
 var $nextButton = document.querySelector('.next-button');
 
 var $detailView = document.querySelector('[data-view="detail"]');
-var $backLinkDet = document.querySelector('.back-arrow-detail');
+var $backLinkDetail = document.querySelector('.back-arrow-detail');
 var $bookmarkAction = document.querySelector('.bookmark-action');
-var $topLinkDet = document.querySelector('.up-arrow');
+var $topLinkDetail = document.querySelector('.up-arrow');
 
 var $searchView = document.querySelector('[data-view="search"]');
 var $form = document.querySelector('form');
-var $searchInput = document.querySelector('input');
-var $usggestionsView = document.querySelector('[data-view="suggestions"]');
+var $input = document.querySelector('input');
+var $suggestionsView = document.querySelector('[data-view="suggestions"]');
 var $suggestions = document.querySelector('.suggestions');
-var $closeButton = document.querySelector('.exit-button');
+var $exitButton = document.querySelector('.exit-button');
 
 var $backLinkToFeatured = document.querySelector('.back-to-featured');
 
-function getData(url) {
+function getData(url, task) {
   var xhr = new XMLHttpRequest();
-
-  if (view === 'featured' && previousView !== 'search') {
-    xhr.open('GET', domain + key + pageParam + pageNumber.toString());
-  } else {
-    xhr.open('GET', url);
-  }
-
+  xhr.open('GET', url);
   xhr.responseType = 'json';
-
-  xhr.addEventListener('load', function (event) {
-    if (view === 'featured') {
-      previousUrl = xhr.response.previous;
-      nextUrl = xhr.response.next;
-      renderCards(xhr.response.results);
-    } else if (view === 'detail') {
-      currentDetail.background_image = xhr.response.background_image;
-      currentDetail.name = xhr.response.name;
-      currentDetail.slug = xhr.response.slug;
-      fillDetail(xhr.response);
-    } else if (view === 'search') {
-      $usggestionsView.hidden = false;
-      timeoutId = null;
-      renderResults(xhr.response.results);
-    }
-  });
-
+  xhr.addEventListener('load', function (event) { task(xhr.response); });
   xhr.send();
 }
 
-function generateDomTree(tagName, attributes, children) {
+// function getData(url) {
+//   var xhr = new XMLHttpRequest();
+
+//   if (view === 'featured' && previousView !== 'search') {
+//     xhr.open('GET', domain + key + pageParam + pageNumber.toString());
+//   } else {
+//     xhr.open('GET', url);
+//   }
+
+//   xhr.responseType = 'json';
+
+//   xhr.addEventListener('load', function (event) {
+//     if (view === 'featured') {
+//       previousUrl = xhr.response.previous;
+//       nextUrl = xhr.response.next;
+//       renderCards(xhr.response.results);
+//     } else if (view === 'detail') {
+//       currentDetail.background_image = xhr.response.background_image;
+//       currentDetail.name = xhr.response.name;
+//       currentDetail.slug = xhr.response.slug;
+//       fillCardDetail(xhr.response);
+//     } else if (view === 'search') {
+//       $suggestionsView.hidden = false;
+//       timeoutId = null;
+//       renderResults(xhr.response.results);
+//     }
+//   });
+
+//   xhr.send();
+// }
+
+function createDomTree(tagName, attributes, children) {
   var $element = document.createElement(tagName);
 
   if (!children) {
@@ -84,7 +93,7 @@ function generateDomTree(tagName, attributes, children) {
       $element.textContent = attributes.textContent;
     } else {
       if (attributes[key] === null) {
-        $element.setAttribute(key, 'https://via.placeholder.com/200x200.jpg?text=+');
+        $element.setAttribute(key, placeholderImage);
       } else {
         $element.setAttribute(key, attributes[key]);
       }
@@ -98,29 +107,41 @@ function generateDomTree(tagName, attributes, children) {
   return $element;
 }
 
-function renderCards(array) {
-  $cards.replaceChildren();
+// FEATURED VIEW
+function renderCards(object) {
+  $featuredGames.replaceChildren();
+
+  for (var i = 0; i < object.results.length; i++) {
+    $featuredGames.appendChild(
+      createDomTree('div', { class: 'card-wrapper col-50' }, [
+        createDomTree('div', {
+          class: 'card-featured row',
+          'data-url': domain + '/' + object.results[i].slug + key
+        }, [
+          createDomTree('div', { class: 'col-100' }, [
+            createDomTree('div', { class: 'row' }, [
+              createDomTree('div', { class: 'card-thumbnail-featured col-100' }, [
+                createDomTree('img', {
+                  src: object.results[i].background_image,
+                  alt: object.results[i].name
+                })])]),
+            createDomTree('div', { class: 'row' }, [
+              createDomTree('div', { class: 'card-title-featured col-100' }, [
+                createDomTree('h4', {
+                  class: 'text-center',
+                  textContent: object.results[i].name
+                })])])])])]));
+  }
+}
+
+// DETAIL VIEW
+function renderDetailList(parentElement, array) {
+  parentElement.replaceChildren();
 
   for (var i = 0; i < array.length; i++) {
-    $cards.appendChild(
-      generateDomTree('div', { class: 'card-wrapper col-50' }, [
-        generateDomTree('div', {
-          class: 'card-featured row',
-          'data-url': domain + '/' + array[i].slug + key
-        }, [
-          generateDomTree('div', { class: 'col-100' }, [
-            generateDomTree('div', { class: 'row' }, [
-              generateDomTree('div', { class: 'card-thumbnail-featured col-100' }, [
-                generateDomTree('img', {
-                  src: array[i].background_image,
-                  alt: array[i].name
-                })])]),
-            generateDomTree('div', { class: 'row' }, [
-              generateDomTree('div', { class: 'card-title-featured col-100' }, [
-                generateDomTree('h4', {
-                  class: 'text-center',
-                  textContent: array[i].name
-                })])])])])]));
+    parentElement.appendChild(
+      createDomTree('li', { textContent: array[i].name })
+    );
   }
 }
 
@@ -138,14 +159,11 @@ function fillDetail(object) {
   $banner.src = object.background_image;
   $banner.alt = object.name;
   $about.textContent = object.description_raw;
-  $genre.replaceChildren();
-  addListElements($genre, object.genres);
   $releaseDate.textContent = object.released;
-  $developer.replaceChildren();
-  addListElements($developer, object.developers);
-  $publisher.replaceChildren();
-  addListElements($publisher, object.publishers);
   $website.href = object.website;
+  renderDetailList($genre, object.genres);
+  renderDetailList($developer, object.developers);
+  renderDetailList($publisher, object.publishers);
 
   if (object.esrb_rating !== null) {
     $esrbRating.textContent = object.esrb_rating.name;
@@ -153,24 +171,79 @@ function fillDetail(object) {
     $esrbRating.textContent = '';
   }
 
-  var indexOfBookmark = data.bookmarks.findIndex(function (object) {
-    return object.slug === currentDetail.slug;
-  });
+  // var indexOfBookmark = data.bookmarks.findIndex(function (object) {
+  //   return object.slug === currentDetail.slug;
+  // });
 
-  if (indexOfBookmark === -1) {
-    $bookmarkAction.className = 'bookmark-action far fa-bookmark';
+  // if (indexOfBookmark === -1) {
+  //   $bookmarkAction.className = 'bookmark-action far fa-bookmark';
+  // } else {
+  //   $bookmarkAction.className = 'bookmark-action fas fa-bookmark';
+  // }
+} // fix
+
+$featuredGames.addEventListener('click', function (event) {
+  if (event.target.closest('.card-featured')) {
+    getData(event.target.closest('.card-featured').getAttribute('data-url'), fillDetail);
+    window.scrollTo({ top: 0, behavior: 'instant' });
+    $featuredView.hidden = true;
+    $detailView.hidden = false;
+    view = 'detail';
+  }
+});
+
+// SEARCH VIEW
+function renderResults(object) {
+  $suggestions.replaceChildren();
+
+  for (var i = 0; i < object.results.length; i++) {
+    if (i === 10) {
+      return;
+    }
+
+    $suggestions.appendChild(
+      createDomTree('li', {}, [
+        createDomTree('a', {
+          href: '#',
+          'data-url': domain + '/' + object.results[i].slug + key,
+          textContent: object.results[i].name
+        })]));
+  }
+}
+
+$input.addEventListener('keyup', function (event) {
+  if (event && timeoutId !== null) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      getData(domain + key + searchParam + $input.value, renderResults);
+    }, 500);
   } else {
-    $bookmarkAction.className = 'bookmark-action fas fa-bookmark';
+    timeoutId = setTimeout(function () {
+      getData(domain + key + searchParam + $input.value, renderResults);
+    }, 500);
   }
-}
 
-function addListElements(parentElement, array) {
-  for (var i = 0; i < array.length; i++) {
-    var child = document.createElement('li');
-    child.textContent = array[i].name;
-    parentElement.appendChild(child);
+  $suggestionsView.hidden = false;
+});
+
+$suggestions.addEventListener('click', function (event) {
+  if (event.target.matches('a')) {
+    getData(event.target.getAttribute('data-url'), fillDetail);
+    $featuredView.hidden = true;
+    $detailView.hidden = false;
+    $searchView.hidden = true;
   }
-}
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  getData(domain + key + searchParam + $input.value, renderCards);
+  $viewLabel.textContent = 'Search Results';
+  $backLinkToFeatured.hidden = false;
+  $featuredView.hidden = false;
+  $detailView.hidden = true;
+  $searchView.hidden = true;
+}); // fix
 
 function toggleModal(event) {
   if (view !== 'search') {
@@ -182,154 +255,96 @@ function toggleModal(event) {
     view = previousView;
   }
 
-  $searchInput.value = null;
+  $input.value = null;
   $suggestions.replaceChildren();
-  $usggestionsView.hidden = true;
+  $suggestionsView.hidden = true;
 }
-
-function renderResults(array) {
-  $suggestions.replaceChildren();
-
-  for (var i = 0; i < array.length; i++) {
-    if (i === 10) {
-      return;
-    }
-
-    $suggestions.appendChild(
-      generateDomTree('li', {},
-        [generateDomTree('a', {
-          href: '#',
-          'data-url': domain + '/' + array[i].slug + key,
-          textContent: array[i].name
-        })]));
-  }
-}
-
-$cards.addEventListener('click', function (event) {
-  if (event.target.closest('.card-featured')) {
-    view = 'detail';
-    $featuredView.hidden = true;
-    $detailView.hidden = false;
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    getData(event.target.closest('.card-featured').getAttribute('data-url'));
-  }
-});
-
-$backButton.addEventListener('click', function (event) {
-  pageNumber--;
-  $pageNumTop.textContent = pageNumber;
-  $pageNumBot.textContent = pageNumber;
-  $nextButton.hidden = false;
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-  getData(previousUrl);
-
-  if (pageNumber === 1) {
-    $backButton.hidden = true;
-  }
-});
-
-$nextButton.addEventListener('click', function (event) {
-  pageNumber++;
-  $pageNumTop.textContent = pageNumber;
-  $pageNumBot.textContent = pageNumber;
-  $backButton.hidden = false;
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-  getData(nextUrl);
-
-  if (nextUrl === null) {
-    $nextButton.hidden = true;
-  }
-});
-
-function toFeatured(event) {
-  view = 'featured';
-  $featuredView.hidden = false;
-  $detailView.hidden = true;
-  getData();
-}
-
-$backLinkDet.addEventListener('click', toFeatured);
-$backLinkFeat.addEventListener('click', toFeatured);
-
-$topLinkDet.addEventListener('click', function (event) {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
 
 $searchIcon.addEventListener('click', toggleModal);
-$closeButton.addEventListener('click', toggleModal);
+$exitButton.addEventListener('click', toggleModal);
 
-$searchInput.addEventListener('keyup', function (event) {
-  var searchUrl = domain + key + searchParam + $searchInput.value;
+// NAVIGATION
 
-  if (event && timeoutId !== null) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () { getData(searchUrl); }, 500);
-  } else {
-    timeoutId = setTimeout(function () { getData(searchUrl); }, 500);
-  }
-});
+// $backButton.addEventListener('click', function (event) {
+//   pageNumber--;
+//   $pageNumTop.textContent = pageNumber;
+//   $pageNumBot.textContent = pageNumber;
+//   $nextButton.hidden = false;
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+//   getData(previousUrl);
 
-$suggestions.addEventListener('click', function (event) {
-  if (event.target.matches('a')) {
-    view = 'detail';
-    $featuredView.hidden = true;
-    $detailView.hidden = false;
-    $searchView.hidden = true;
-    getData(event.target.getAttribute('data-url'));
-  }
-});
+//   if (pageNumber === 1) {
+//     $backButton.hidden = true;
+//   }
+// });
 
-$form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var searchUrl = domain + key + searchParam + $searchInput.value;
-  previousView = view;
-  view = 'featured';
-  $viewLabel.textContent = 'Search Results';
-  $backLinkToFeatured.hidden = false;
-  $featuredView.hidden = false;
-  $detailView.hidden = true;
-  $searchView.hidden = true;
-  getData(searchUrl);
-});
+// $nextButton.addEventListener('click', function (event) {
+//   pageNumber++;
+//   $pageNumTop.textContent = pageNumber;
+//   $pageNumBot.textContent = pageNumber;
+//   $backButton.hidden = false;
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+//   getData(nextUrl);
 
-$backLinkToFeatured.addEventListener('click', function (event) {
-  $viewLabel.textContent = 'Featured';
-  $backLinkToFeatured.hidden = true;
-  $pageLabel.hidden = false;
-  $pageNumTop.hidden = false;
-  $backButton.hidden = true;
-  $pageNumBot.hidden = false;
-  $nextButton.hidden = false;
-  $cards.replaceChildren();
-  getData(domain + key + pageParam + pageNumber.toString());
-});
+//   if (nextUrl === null) {
+//     $nextButton.hidden = true;
+//   }
+// });
 
-$bookmarkIconHeader.addEventListener('click', function (event) {
-  view = 'featured';
-  $viewLabel.textContent = 'Bookmarks';
-  $backLinkToFeatured.hidden = false;
-  $featuredView.hidden = false;
-  $detailView.hidden = true;
-  $pageLabel.hidden = true;
-  $pageNumTop.hidden = true;
-  $backButton.hidden = true;
-  $pageNumBot.hidden = true;
-  $nextButton.hidden = true;
-  renderCards(data.bookmarks);
-});
+// function toFeatured(event) {
+//   view = 'featured';
+//   $featuredView.hidden = false;
+//   $detailView.hidden = true;
+//   getData();
+// }
 
-$bookmarkAction.addEventListener('click', function (event) {
-  var indexOfBookmark = data.bookmarks.findIndex(function (object) {
-    return object.slug === currentDetail.slug;
-  });
+// $backLinkDetail.addEventListener('click', toFeatured);
+// $backLinkFeat.addEventListener('click', toFeatured);
 
-  if (indexOfBookmark === -1) {
-    $bookmarkAction.className = 'bookmark-action fas fa-bookmark';
-    data.bookmarks.push(currentDetail);
-  } else {
-    $bookmarkAction.className = 'bookmark-action far fa-bookmark';
-    data.bookmarks.splice(indexOfBookmark, 1);
-  }
-});
+// $topLinkDetail.addEventListener('click', function (event) {
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
 
-getData();
+// $backLinkToFeatured.addEventListener('click', function (event) {
+//   $viewLabel.textContent = 'Featured';
+//   $backLinkToFeatured.hidden = true;
+//   $pageLabel.hidden = false;
+//   $pageNumTop.hidden = false;
+//   $backButton.hidden = true;
+//   $pageNumBot.hidden = false;
+//   $nextButton.hidden = false;
+//   $featuredGames.replaceChildren();
+//   getData(domain + key + pageParam + pageNumber.toString());
+// });
+
+// $bookmarkIconHeader.addEventListener('click', function (event) {
+//   view = 'featured';
+//   $viewLabel.textContent = 'Bookmarks';
+//   $backLinkToFeatured.hidden = false;
+//   $featuredView.hidden = false;
+//   $detailView.hidden = true;
+//   $pageLabel.hidden = true;
+//   $pageNumTop.hidden = true;
+//   $backButton.hidden = true;
+//   $pageNumBot.hidden = true;
+//   $nextButton.hidden = true;
+//   renderCards(data.bookmarks);
+// });
+
+// $bookmarkAction.addEventListener('click', function (event) {
+//   var indexOfBookmark = data.bookmarks.findIndex(function (object) {
+//     return object.slug === currentDetail.slug;
+//   });
+
+//   if (indexOfBookmark === -1) {
+//     $bookmarkAction.className = 'bookmark-action fas fa-bookmark';
+//     data.bookmarks.push(currentDetail);
+//   } else {
+//     $bookmarkAction.className = 'bookmark-action far fa-bookmark';
+//     data.bookmarks.splice(indexOfBookmark, 1);
+//   }
+// });
+
+// getData();
+
+getData(domain + key + pageParam + pageNumber, renderCards);
